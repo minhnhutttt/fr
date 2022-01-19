06967aa6658ca5ecabf7ee7c5b4324124ee7da7f
Minh nhut <nhut@rabi.co.jp>
2022-01-19 17:13:41 +0700

    2

--- fr/main.js
+++ fr/main.js
@@ -1,6 +1,82 @@
-import './style.css'
+import * as THREE from "https://cdn.skypack.dev/three@0.126.1";
+import * as dat from "dat.gui";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSeqments: 10,
+    heightSeqments: 10,
+  },
+};
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+
+gui.add(world.plane, "widthSeqments", 1, 50).onChange(generatePlane);
+
+gui.add(world.plane, "heightSeqments", 1, 50).onChange(generatePlane);
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthSeqments,
+    world.plane.heightSeqments
+  );
+  const { array } = planeMesh.geometry.attributes.position;
+
+  for (let i = 0; i < array.length; i += 3) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    array[i + 2] = z + Math.random();
+  }
+}
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  innerWidth / innerHeight,
+  0.1,
+  1000
+);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+camera.position.z = 5;
+
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
+});
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+
+const { array } = planeMesh.geometry.attributes.position;
+
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+
+  array[i + 2] = z + Math.random();
+}
+
+const light = new THREE.DirectionalLight(0xfffff, 1);
+light.position.set(0, 0, 1);
+
+scene.add(light);
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
